a6065517af52423925d0a23c56ae29aef743d06f
thangtienql <thangtienql@gmail.com>
2021-08-04 12:10:14 +0700

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5c3dfd085cd88375ac1b273acb13b8a3'),
+    },
+  },
+});
